c3436dbe160c79175579df370f93021f88ee651e
kWinCode <kevin.pirner@live.at>
2021-04-08 13:47:13 +0200

    added possibility to change settigns, react default props and callbacks

--- api/views.py
+++ api/views.py
@@ -100,10 +100,13 @@ class LeaveRoom(APIView):
 
 class UpdateRoom(APIView):
     serializer_class = UpdateRoomSerializer
+
     def patch(self, request, format=None):
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
+
         serializer = self.serializer_class(data=request.data)
+        print (serializer, serializer.is_valid())
         if serializer.is_valid():
             guest_can_pause = serializer.data.get('guest_can_pause')
             votes_to_skip = serializer.data.get('votes_to_skip')
