0cd74826ab44f62f14fb01a1a7928e7de4afb00f
kWinCode <kevin.pirner@live.at>
2021-04-08 17:18:59 +0200

    connected to spotify api, user can now connect account, checked in frontend if connected

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -9,7 +9,8 @@ export default class Room extends Component {
             votesToSkip: 2,
             guestCanPause: false,
             isHost: false,
-            showSettings: false
+            showSettings: false,
+            spotifyAuthenticated: false,
         };
         this.roomCode = this.props.match.params.roomCode;
         this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
@@ -17,6 +18,7 @@ export default class Room extends Component {
         this.renderSettingsButton = this.renderSettingsButton.bind(this);
         this.renderSettings = this.renderSettings.bind(this);
         this.getRoomDetails = this.getRoomDetails.bind(this);
+        this.authenticateSpotify = this.authenticateSpotify.bind(this);
         this.getRoomDetails();
     }
 
@@ -36,9 +38,29 @@ export default class Room extends Component {
                 guestCanPause: data.guest_can_pause,
                 isHost: data.is_host,
             });
+            if (this.state.isHost) {
+              this.authenticateSpotify();
+            }
         });
     }
-
+    
+    authenticateSpotify() {
+      fetch('/spotify/is-authenticated')
+        .then( response => response.json())
+        .then( data => {
+          this.setState({spotifyAuthenticated: data.status});
+          if (!data.status) {
+            fetch('/spotify/get-auth-url')
+                .then( response => response.json())
+                // if not authenticated -> redirect to spotify authentication page
+                // after user is authenticated via backend, the user will be redirected by our backend to our frontend
+                // redirect url is stored in credentials.py
+                .then( data => {
+                    window.location.replace(data.url);
+                });
+          }
+        });
+    }
 
     leaveButtonPressed() {
         const requestOptions = {
